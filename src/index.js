50dec0700ce2ca407114f79eb904c1019fd14114
Yura <pumpkteam3@gmail.com>
2022-05-26 19:00:02 +0300

    added component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const h1 = <h1>Hello world</h1>;
+function App () {
+    return <h1>Hello app.js</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+root.render(<App />);
