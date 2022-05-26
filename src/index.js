7b41ae7bc9042de847b3af6e0c3f567c531af4d1
Yura <pumpkteam3@gmail.com>
2022-05-26 19:06:46 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-function App () {
-    return <h1>Hello app.js</h1>
+function App() {
+  return (
+  <>
+    <h1>Hello app.js</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
+  </>);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
