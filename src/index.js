eb91202178de7e7528a724cda69b474046f1e29e
Yura <pumpkteam3@gmail.com>
2022-05-26 20:02:17 +0300

    added components

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function Header() {
-    return (
-        <h1>
-            <h1>Hello app.js</h1>
-        </h1>
-    )
-}
-
-function AppList() {
-    return (
-        <ul>
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-        </ul>
-    )
-}
-
-function App() {
-    return (
-        <>
-            <Header />
-            <AppList />
-        </>
-    )
-}
+import { App } from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(<App />)
