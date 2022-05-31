75778cb9d20ac1cadc6b440474622417f9d5271f
Yura <pumpkteam3@gmail.com>
2022-05-31 23:41:23 +0300

    add material-ui

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -3,10 +3,12 @@ import React from 'react'
 import { Header } from 'container/Header/Header'
 import { Main } from 'container/Main/Main'
 import { Footer } from 'container/Footer/Footer'
+import { CssBaseline } from '@mui/material'
 
 export const App = () => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
