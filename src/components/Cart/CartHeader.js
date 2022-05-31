176037aa971a9a83cc47e36b8f4946796da4e7dd
Yura <pumpkteam3@gmail.com>
2022-06-01 00:08:05 +0300

    css styling var 1

---
+++ src/components/Cart/CartHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './CartHeader.css'
+
+export const CartHeader = () => {
+    return (
+        <div className="CartHeader">
+            <div>0</div>
+            <div>0$</div>
+        </div>
+    )
+}
