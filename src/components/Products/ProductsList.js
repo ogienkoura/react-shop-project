4a320e2db487ebcad9cbefe6976f4fe7f684e694
Yura <pumpkteam3@gmail.com>
2022-06-02 19:14:52 +0300

    added cart items with array

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import { ProductListItem } from './ProductListItem'
+import productsArray from 'utils/poductsArray'
 
 export const ProductsList = () => {
     return (
@@ -23,33 +24,17 @@ export const ProductsList = () => {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone XS"
-                        description="This is iPhone XS"
-                        type="phone"
-                        capacity="64"
-                        price="500"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone 13 Pro"
-                        description="This is iPhone 13 Pro"
-                        type="phone"
-                        capacity="128"
-                        price="1000"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone 12"
-                        description="This is iPhone 12"
-                        type="phone"
-                        capacity="32"
-                        price="400"
-                    />
-                </Grid>
+                {productsArray.map((product) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ProductListItem
+                            name={product.name}
+                            description={product.description}
+                            type={product.type}
+                            capacity={product.capacity}
+                            price={product.price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
---
+++ src/utils/poductsArray.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        name: 'iPhone 13 Pro',
+        description: 'This is iPhone 13 Pro',
+        type: 'phone',
+        capacity: '128',
+        price: '1000',
+    },
+    {
+        name: 'iPhone 12',
+        description: 'This is iPhone 12',
+        type: 'phone',
+        capacity: '64',
+        price: '800',
+    },
+    {
+        name: 'iPhone 11 Pro Max',
+        description: 'This is iPhone 11 Pro Max',
+        type: 'phone',
+        capacity: '256',
+        price: '700',
+    },
+    {
+        name: 'Nokia 6151',
+        description: 'This is Nokia 6151',
+        type: 'phone',
+        capacity: '32',
+        price: '200',
+    },
+    {
+        name: 'Siemens 123',
+        description: 'This is Siemens 123',
+        type: 'phone',
+        capacity: '8',
+        price: '150',
+    },
+    {
+        name: 'Sony Xperia',
+        description: 'This is Sony Xperia',
+        type: 'phone',
+        capacity: '64',
+        price: '250',
+    },
+]
+
+export default products
