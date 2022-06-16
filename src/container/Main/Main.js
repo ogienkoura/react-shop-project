96b1f1c5a3f7ffd1ce81c136b8c8713f0273aa7d
ogienkoura <pumpkteam3@gmail.com>
2022-06-17 01:14:48 +0300

    add react-router

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Container } from '@mui/system'
 import { ProductsList } from 'components/Products/ProductsList'
+import { Route, Routes } from 'react-router-dom'
 
 export const Main = ({addProductToCart}) => {
     return (
         <>
             <Container>
-                <ProductsList addProductToCart={addProductToCart}/>
+
+                <Routes>
+                    <Route path="/" element={<ProductsList addProductToCart={addProductToCart}/>} />
+                </Routes>
+                
             </Container>
         </>
     )
