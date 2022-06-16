5fcca0b56e36490549a7e2cd2cce0568ce716a2c
ogienkoura <pumpkteam3@gmail.com>
2022-06-17 02:13:33 +0300

    add menu pages

---
+++ src/pages/Payment/PaymentPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PaymentPage = () => {
+    return <h1>Payment page</h1>
+}
---
+++ src/pages/Shipping/ShippingPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ShippingPage = () => {
+    return <h1>Shipping page</h1>
+}
