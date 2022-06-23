938cfd704d440820fab23648f6ac231bd5a12f74
Yura <pumpkteam3@gmail.com>
2022-06-23 20:43:42 +0300

    add review to Review

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,13 +1,40 @@
-import { Card, TextField } from '@mui/material'
+import { Button, Card, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import { arrReviews } from './arrReviews'
 import './Reviews.scss'
 
 export const Reviews = () => {
     const [reviews, setReviews] = useState(arrReviews)
+    const [newReview, setNewReview] = useState({
+        name: '',
+        text: '',
+    })
+
+    const handleName = (e) =>
+        setNewReview((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+
+    const handleText = (e) =>
+        setNewReview((prevState) => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+
+    const reviewSend = (e) => {
+        e.preventDefault()
+        setNewReview({
+            name: '',
+            text: '',
+        })
+        setReviews((value) => {
+            return [...value, newReview]
+        })
+    }
 
     return (
-        <>
+        <div className="reviews-block">
             <h2>Reviews</h2>
             {reviews.map(({ name, text }, i) => (
                 <Card className="reviews-block-item" key={i}>
@@ -17,14 +44,29 @@ export const Reviews = () => {
             ))}
             <h3>Please leave review about this product</h3>
 
-            <form>
+            <form onSubmit={reviewSend}>
                 <div>
-                    <TextField size="small" label="Name" margin="normal" />
+                    <TextField
+                        size="small"
+                        label="Name"
+                        margin="normal"
+                        value={newReview.name}
+                        onChange={handleName}
+                    />
                 </div>
                 <div>
-                    <TextField size="small" label="Text" margin="normal" />
+                    <TextField
+                        size="small"
+                        label="Text"
+                        margin="normal"
+                        value={newReview.text}
+                        onChange={handleText}
+                    />
                 </div>
+                <Button variant="contained" onClick={reviewSend}>
+                    Leave comment
+                </Button>
             </form>
-        </>
+        </div>
     )
 }
